a9e7f42960ac4c55123e2dbab8885061d5dbad90
utakata1024 <tty1024kit@gmail.com>
2025-09-13 16:54:59 +0900

    クリア後も弾発射できるバグの修正

--- Assets/Scenes/game_scene_each/utkt's_scene/Shoot.cs
+++ Assets/Scenes/game_scene_each/utkt's_scene/Shoot.cs
@@ -2,25 +2,26 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
-    public GameObject bulletPrefab;     // 弾のプレハブ
-    public Transform firePoint;         // 弾の発射位置
-    public float fireRate = 0.5f;       // 発射間隔（秒）
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float fireRate = 0.5f;
     private float nextFireTime = 0f;
 
     void Update()
     {
-        // 弾の発射
-        if (Input.GetButtonDown("Fire1") && Time.time > nextFireTime)
+        // ゲームがクリア状態ではない場合のみ、以下の処理を実行する
+        if (!GameManager.isGameClear)
         {
-            nextFireTime = Time.time + fireRate;
-            Shoot();
+            if (Input.GetButtonDown("Fire1") && Time.time > nextFireTime)
+            {
+                nextFireTime = Time.time + fireRate;
+                Shoot();
+            }
         }
     }
 
-    // 弾を発射する関数
     void Shoot()
     {
-        // 弾を生成
         Instantiate(bulletPrefab, firePoint.position, transform.rotation);
     }
 }
