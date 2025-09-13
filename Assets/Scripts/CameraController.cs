0c4953ad0fb55b5660a9a78c645aa8c4d9994b16
R1Jin <kyutech_jk@icloud.com>
2025-09-13 22:23:55 +0900

    add: CameraController.csの修正

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,10 +8,22 @@ public class CameraController : MonoBehaviour
     // カメラがこれ以上左に動かないようにする限界位置
     private float minXPosition;
 
+    //カメラがこれ以上上に動かないようにする限界位置
+    private float maxYPosition;
+
+    //カメラがこれ以上下に動かないようにする限界位置
+    private float minYPosition;
+
+    //カメラの最終的な位置
+    private float CameraPosition;
+
     void Start()
     {
-        // ゲーム開始時にカメラの左端の座標を設定してください
+        // ゲーム開始時のカメラの左端、上限、下限の座標を設定してください
+        //数字の後ろにfつけないと動きません
         minXPosition = -7012f;
+        maxYPosition = 1000f;
+        minYPosition = 0f;
     }
 
     void LateUpdate()
@@ -23,14 +35,18 @@ public class CameraController : MonoBehaviour
             // カメラのZ座標は、元のZ座標を維持する
 
             // カメラが目指す新しい位置を計算
-            Vector3 targetPosition = new Vector3(player.position.x, 680f, transform.position.z);
+            Vector3 targetPosition = new Vector3(player.position.x, player.position.y, transform.position.z);
 
             // プレイヤーが左に戻っても、カメラがminXPositionよりも左に動かないようにする
             // Mathf.Maxを使って、targetPosition.xとminXPositionの大きい方を採用
             float clampedX = Mathf.Max(targetPosition.x, minXPosition);
 
             // clampedXを使って新しい位置を再設定
-            targetPosition = new Vector3(clampedX, 680f, transform.position.z);
+            targetPosition = new Vector3(clampedX, CameraPosition, transform.position.z);
+
+            /*カメラの位置が上限、下限を割りそうになったら上限、下限で止める。
+            そうでなければプレイヤー追従*/
+            CameraPosition = Mathf.Clamp(targetPosition.y, minYPosition, maxYPosition);
 
             // カメラの位置を徐々に目標位置に移動させる
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
