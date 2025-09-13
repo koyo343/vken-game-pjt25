1013797056194526ddf7c0315c47b81c5f8401ea
koara29794 <20kari02.09taku06@gmail.com>
2025-09-13 15:44:42 +0900

    ゲーム内UIがほぼ完成15:44

--- Assets/Scenes/game_scene_each/koara's_scene/PauseController.cs
+++ Assets/Scenes/game_scene_each/koara's_scene/PauseController.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 public class PauseController : MonoBehaviour
 {
     [Header("ポーズ画面のUI")]
-    public GameObject pausePanel; // インスペクターからPausePanelをアタッチする
+    public GameObject PausePanel; // インスペクターからPausePanelをアタッチする
+
+    public GameObject SettingsPanel; // インスペクターからSettingsPanelをアタッチする
 
     private bool isPaused = false; // ポーズ状態を管理するフラグ
+
+    private bool issetting = false; // 設定画面を管理するフラグ
 
     void Update()
     {
@@ -29,7 +33,7 @@ public class PauseController : MonoBehaviour
     public void PauseGame()
     {
         // ポーズ画面をアクティブにする
-        pausePanel.SetActive(true);
+        PausePanel.SetActive(true);
         // ゲーム内の時間を停止する
         Time.timeScale = 0f;
         // ポーズ状態フラグをtrueにする
@@ -40,10 +44,32 @@ public class PauseController : MonoBehaviour
     public void ResumeGame()
     {
         // ポーズ画面を非アクティブにする
-        pausePanel.SetActive(false);
+        PausePanel.SetActive(false);
         // ゲーム内の時間を元に戻す
         Time.timeScale = 1f;
         // ポーズ状態フラグをfalseにする
         isPaused = false;
+    }
+
+    // ■ 設定画面に入る処理
+    public void SettingGame()
+    {
+        // 設定画面をアクティブにする
+        SettingsPanel.SetActive(true);
+        // ポーズ画面を非アクティブにする
+        PausePanel.SetActive(false);
+        // 設定状態フラグをtrueにする
+        issetting = true;
+    }
+
+    // ■ ポーズ画面に戻る処理
+    public void ResumePause()
+    {
+        // 設定画面を非アクティブにする
+        SettingsPanel.SetActive(false);
+        // ポーズ画面を非アクティブにする
+        PausePanel.SetActive(true);
+        // 設定状態フラグをfalseにする
+        issetting = false;
     }
 }
