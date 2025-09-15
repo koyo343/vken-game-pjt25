533deecb321d43f7c749a1a6c545868fd99cddc5
R1Jin <kyutech_jk@icloud.com>
2025-09-15 15:53:46 +0900

    見えない壁の実装および落下ロジックの更新

--- Assets/Scenes/game_scene_each/rijin's_scene/InvisibleWallController.cs
+++ Assets/Scenes/game_scene_each/rijin's_scene/InvisibleWallController.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class InvisibleWallController : MonoBehaviour
 {
     public Camera mainCamera;
-    public Transform InvisibleWall;
+    public Transform InvisibleWall_Left;//もちろん左端
+    public Transform InvisibleWall_Right;//もちろん右端
+    public Transform InvisibleWall_Up;//もちろん天井
 
     public Transform Player;
 
@@ -29,15 +31,21 @@ public class InvisibleWallController : MonoBehaviour
         // カメラの左端のワールド座標をViewportToWorldPointで取得
         Vector3 leftEdge = mainCamera.ViewportToWorldPoint(new Vector3(0f, 0.5f, mainCamera.nearClipPlane));
 
-        Debug.Log("カメラの左端のX座標: " + leftEdge.x);
+        // カメラの右端のワールド座標をViewportToWorldPointで取得
+        Vector3 rightEdge = mainCamera.ViewportToWorldPoint(new Vector3(1f, 0.5f, mainCamera.nearClipPlane));
 
-        if (InvisibleWall == null || Player == null)
+        // カメラの上端のワールド座標をViewportToWorldPointで取得
+        Vector3 upEdge = mainCamera.ViewportToWorldPoint(new Vector3(0f, 1f, mainCamera.nearClipPlane));
+
+        if (InvisibleWall_Left == null || InvisibleWall_Right == null || InvisibleWall_Up == null || Player == null)
         {
             Debug.Log("InvisibleWallまたはPlayerがセットされていません");
         }
         else
         {
-            InvisibleWall.position = new Vector3(leftEdge.x, Player.position.y, Player.position.z);
+            InvisibleWall_Left.position = new Vector3(leftEdge.x, Player.position.y, Player.position.z);
+            InvisibleWall_Right.position = new Vector3(rightEdge.x, Player.position.y, Player.position.z);
+            InvisibleWall_Up.position = new Vector3(Player.position.x, upEdge.y, Player.position.z);
         }
     }
 }
