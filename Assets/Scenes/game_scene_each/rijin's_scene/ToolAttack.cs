b8e742c0ce576fa791536e860db6ccc25e5ef2d7
R1Jin <kyutech_jk@icloud.com>
2025-09-29 00:09:27 +0900

    攻撃に対する敵の当たり判定力学の修正

--- Assets/Scenes/game_scene_each/rijin's_scene/ToolAttack.cs
+++ Assets/Scenes/game_scene_each/rijin's_scene/ToolAttack.cs
@@ -2,14 +2,92 @@ using UnityEngine;
 
 public class ToolAttack : MonoBehaviour
 {
-    public AttackManager attackManager;
-    private void AttackDamage(Collider other)
+    public GameData_Manager GameData_Manager;
+    
+    public Ken_Attack ken_Attack;
+
+    public Mito_Attack mito_Attack;
+
+    public Sora_Attack sora_Attack;
+
+    public Uruha_Attack uruha_Attack;
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        string selectedCharacter = GameData_Manager.Instance.selectedCharacter;
+
         DamageAble damageable = other.GetComponent<DamageAble>();
 
-        if (damageable != null && attackManager.isUsingTool)
+        switch (selectedCharacter)
         {
-            damageable.Damage(100);
+            case "剣持刀也":
+
+                Debug.Log("衝突検知:" + other.GetComponent<DamageAble>());
+
+                if (damageable != null)
+                {
+                    Debug.Log("DamageAbleコンポーネント取得");
+                }
+
+                if (damageable != null && sora_Attack.isUsingTool)
+                {
+                    Debug.Log("ダメージ処理の実行");
+                    damageable.Damage(100);
+                }
+
+                break;
+
+            case "月ノ美兎":
+
+                Debug.Log("衝突検知:" + other.GetComponent<DamageAble>());
+
+                if (damageable != null)
+                {
+                    Debug.Log("DamageAbleコンポーネント取得");
+                }
+
+                if (damageable != null && mito_Attack.isUsingTool)
+                {
+                    Debug.Log("ダメージ処理の実行");
+                    damageable.Damage(100);
+                }
+
+                break;
+
+            case "ときのそら":
+
+                Debug.Log("衝突検知:" + other.GetComponent<DamageAble>());
+
+                if (damageable != null)
+                {
+                    Debug.Log("DamageAbleコンポーネント取得");
+                }
+
+                if (damageable != null && sora_Attack.isUsingTool)
+                {
+                    Debug.Log("ダメージ処理の実行");
+                    damageable.Damage(100);
+                }
+
+                break;
+
+            case "一ノ瀬うるは":
+
+                Debug.Log("衝突検知:" + other.GetComponent<DamageAble>());
+
+                if (damageable != null)
+                {
+                    Debug.Log("DamageAbleコンポーネント取得");
+                }
+
+                if (damageable != null && uruha_Attack.isUsingTool)
+                {
+                    Debug.Log("ダメージ処理の実行");
+                    damageable.Damage(100);
+                }
+
+                break;
+
         }
     }
 
