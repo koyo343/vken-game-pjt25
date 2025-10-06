548caafedfd7ced85006087bcc2626933ec12985
Clicode <kou.101530@gmail.com>
2025-10-06 09:16:07 +0900

    fix:bulletの向きを調整及びそれに伴ったプレイヤーの向き更新ロジックの変更

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -126,11 +126,11 @@ public class PlayerController : MonoBehaviour
         {
             if (moveInput < 0)
             {
-                transform.eulerAngles = new Vector3(0, 180, 0);
+                 transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
             }
             else if (moveInput > 0)
             {
-                transform.eulerAngles = new Vector3(0, 0, 0);
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
             }
         }
     }
