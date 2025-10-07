e412926aed948adb1cc1977b377558232195e48c
Clicode <kou.101530@gmail.com>
2025-10-08 03:16:15 +0900

    fix:点滅ロジック変更によりplayerが死ぬバグを修正

--- Assets/Scripts/Player/InvinsibleManager.cs
+++ Assets/Scripts/Player/InvinsibleManager.cs
@@ -1,17 +1,18 @@
 using UnityEngine;
-using System.Collections;//コルーチンを使用するため
+using System.Collections;
+using Mono.Cecil.Cil;//コルーチンを使用するため
 
 public class InvinsibleManager : MonoBehaviour
 {
     //無敵を付与されているか確認するため
     public bool isInvinsible;
-    //アニメーターの情報を入れる変数
-    public Animator animator;
+    private SpriteRendererBlinker blinker;
 
     void Start()
     {
         //非無敵状態にセットする
         isInvinsible = false;
+        blinker = GetComponent<SpriteRendererBlinker>();
     }
 
     public IEnumerator Invinsible(float delaytime)
@@ -21,7 +22,7 @@ public class InvinsibleManager : MonoBehaviour
             //無敵付与
             isInvinsible = true;
             //アニメーターを無敵の状態にする
-            animator.SetBool("isInvinsible", true);
+            blinker.BeginBlink();
 
             //コルーチンで指定秒数待機
             yield return new WaitForSeconds(delaytime);
@@ -29,7 +30,7 @@ public class InvinsibleManager : MonoBehaviour
             //無敵解除
             isInvinsible = false;
             //アニメーターの無敵状態を切る
-            animator.SetBool("isInvinsible", false);
+            blinker.EndBlink();
         }
     }
 }
