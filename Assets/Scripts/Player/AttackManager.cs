6d0cc713d984d2995ebfef03dea2dba7e4f6e316
koara29794 <20kari02.09taku06@gmail.com>
2025-10-08 22:48:50 +0900

    キャラクターSE実装したよ、ただ通常攻撃のSEの元ファイルの音が小さすぎるよ

--- Assets/Scripts/Player/AttackManager.cs
+++ Assets/Scripts/Player/AttackManager.cs
@@ -96,6 +96,22 @@ public class AttackManager : MonoBehaviour
         {
             toolObject.SetActive(true);
             currentAttackComponent.PerformAttack();
+            //SE再生
+            CharactorSE charactorAudioData = GetComponent<CharactorSE>();
+            SEManager seManager = FindObjectOfType<SEManager>();
+            if (seManager != null) 
+            {
+                seManager.PlaySE(charactorAudioData.AttackSound);
+                Debug.Log($"通常攻撃SEを再生しました: {charactorAudioData.AttackSound.name}");
+            }
+            if(charactorAudioData.AttackSound == null)
+            {
+                Debug.Log("SEがアタッチされていません");
+            }
+            else
+            {
+            Debug.LogWarning("SEManagerが見つからないため、SEを再生できませんでした。");
+            }
         }
 
         //スキルの呼び出し
@@ -110,6 +126,55 @@ public class AttackManager : MonoBehaviour
             else
             {
                 //Debug.Log("skill not ready あと " + SkillRecastManager.recastTime + " - " + SkillRecastManager.currentRecastTime + " 秒");
+            }
+            //SE再生
+            CharactorSE charactorAudioData = GetComponent<CharactorSE>();
+            SEManager seManager = FindObjectOfType<SEManager>();
+            string selectedCharacter = GameData_Manager.Instance.selectedCharacter;
+            if (seManager != null) 
+            {
+                switch (selectedCharacter)
+                {
+                    case "ときのそら":
+                        seManager.PlaySE(charactorAudioData.SoraSkillSound);
+                        Debug.Log($"通常攻撃SEを再生しました: {charactorAudioData.SoraSkillSound.name}");
+                        if(charactorAudioData.SoraSkillSound == null)
+                        {
+                            Debug.Log("SEがアタッチされていません");
+                        }
+                        break;
+
+                    case "月ノ美兎":
+                        seManager.PlaySE(charactorAudioData.MitoSkillSound);
+                        Debug.Log($"通常攻撃SEを再生しました: {charactorAudioData.MitoSkillSound.name}");
+                        if(charactorAudioData.MitoSkillSound == null)
+                        {
+                            Debug.Log("SEがアタッチされていません");
+                        }
+                        break;
+
+                    case "剣持刀也":
+                        seManager.PlaySE(charactorAudioData.KenSkillSound);
+                        Debug.Log($"通常攻撃SEを再生しました: {charactorAudioData.KenSkillSound.name}");
+                        if(charactorAudioData.KenSkillSound == null)
+                        {
+                            Debug.Log("SEがアタッチされていません");
+                        }  
+                        break;
+
+                    case "一ノ瀬うるは":
+                        seManager.PlaySE(charactorAudioData.UruhaSkillSound);
+                        Debug.Log($"通常攻撃SEを再生しました: {charactorAudioData.UruhaSkillSound.name}");
+                        if(charactorAudioData.UruhaSkillSound == null)
+                        {
+                            Debug.Log("SEがアタッチされていません");
+                        }
+                        break;
+                }   
+            }
+            else
+            {
+            Debug.LogWarning("SEManagerが見つからないため、SEを再生できませんでした。");
             }
         }
     }
--- Assets/Scripts/SE/AttackSE.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class AttackSE : MonoBehaviour
-{
-    // Inspectorで設定する、攻撃時の効果音
-    public AudioClip AttackSound;
-}
