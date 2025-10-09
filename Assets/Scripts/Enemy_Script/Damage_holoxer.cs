57e11a81cfcd4a9ba0ec107bd3bc0ffe6b91c577
Clicode <kou.101530@gmail.com>
2025-10-09 19:18:47 +0900

    ref:プレイヤーの攻撃ダメージ調整v1.1

--- Assets/Scripts/Enemy_Script/Damage_holoxer.cs
+++ Assets/Scripts/Enemy_Script/Damage_holoxer.cs
@@ -4,13 +4,31 @@ public class Damage_holoxer : DamageBase
 {
     public GameObject holoxer;
 
+    public override void Damage(int damage)
+    {
+
+        //親クラスの呼び出し
+        base.Damage(damage);
+
+        //死亡判定
+        if (GetHealth() <= 0)
+        {
+            Die();
+        }
+    }
+
     protected override void Die()
     {
         /*ここに敵を倒した際のカウンターの呼び出し処理を追加*/
         if (holoxer != null)
         {
             Destroy(holoxer);
-            Debug.Log("Damage holoxer");
+            Debug.Log("holoxerを倒した");
         }
+    }
+
+    public int GetHealth()
+    {
+        return health;
     }
 }
